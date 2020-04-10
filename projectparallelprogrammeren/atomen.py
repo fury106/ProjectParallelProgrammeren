b9262160cec6cf452036fe64585e919b1ab1e808
Indy Verhoeven <indyverhoeven1@outlook.com>
2020-04-10 16:31:59 +0200

    update 10/04

--- projectparallelprogrammeren/atomen.py
+++ projectparallelprogrammeren/atomen.py
@@ -9,13 +9,37 @@ Deze module bevat alle atomen die nodig zijn in de simulatie.
 """
 
 import numpy as np
+import math
 
 class Atomen:
 	"""Deze klasse bevat enkel de coördinaten van de atomen, omdat er enkel de Lennard Jones potentiaal berekend zal worden."""
 	
 	def __init__(self, aantal):
 		print("Er worden", aantal, "atomen gesimuleerd.") #dient voorlopig als test
+		self.Atomen = np.random.rand(3,aantal) #coordinaten genereren van de atomen
+		print(self.Atomen)
 		
+		
+	def getCoordinate(self, nummerAtoom):
+		""" Deze functie retourneert een lijst met het x, y en z coordinaat van het opgegeven atoom.
+		:param int nummerAtoom: is het nummer (min = 0, max = #atomen - 1) van het atoom waar je de coordinaten van wil weten"""
+		x = self.Atomen[0, nummerAtoom]
+		y = self.Atomen[1, nummerAtoom]
+		z = self.Atomen[2, nummerAtoom]
+		return [x, y, z]
+		
+		
+	def afstandTweeAtomen(self, nummerAtoom1, nummerAtoom2):
+		""" Deze functie berekent de afstand tussen de twee opgegeven atomen.
+		:param int nummerAtoom1: is het nummer van het eerste atoom.
+		:param int nummerAtoom2: is het nummer van het tweede atoom."""
+		atoom1 = self.getCoordinate(nummerAtoom1)
+		atoom2 = self.getCoordinate(nummerAtoom2)
+		afstand = math.sqrt(math.pow((atoom1[0] - atoom2[0]) , 2) + math.pow((atoom1[1] - atoom2[1]) , 2) + math.pow((atoom1[2] - atoom2[2]) , 2))
+		return afstand
 		
 #test code
-test = Atomen(3)
+test = Atomen(8)
+print(test.getCoordinate(2))
+print(test)
+print(test.afstandTweeAtomen(1,2))
--- tests/test_atomen.py
+++ tests/test_atomen.py
@@ -6,18 +6,26 @@
 import os
 import sys
 import pytest
+import math
 
 from click.testing import CliRunner
 from click import echo
 
-import projectparallelprogrammeren 
+import projectparallelprogrammeren
 
+#def test_greet():
+ #   expected = "Hello John!"
+  #  greeting = greet("John")
+   # assert greeting==expected
 
-def test_greet():
-    expected = "Hello John!"
-    greeting = greet("John")
-    assert greeting==expected
-
+def test_afstand():
+	test = Atomen(5)
+	atoom1 = test.getCoordinate(1)
+	atoom2 = test.getCoordinate(2)
+	expected = math.sqrt(math.pow((atoom1[0] - atoom2[0]) , 2) + math.pow((atoom1[1] - atoom2[1]) , 2) + math.pow((atoom1[2] - atoom2[2]) , 2))
+	afstand = test.afstandTweeAtomen(1, 2)
+	assert afstand == expected
+	
 
 # ==============================================================================
 # The code below is for debugging a particular test in eclipse/pydev.
